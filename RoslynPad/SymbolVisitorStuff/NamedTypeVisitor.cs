31e304ada25a7eba2ee510ca5b1ec317f6e46e34
Josh Varty <joshvarty@gmail.com>
2015-10-19 14:19:52 -0400

    PrettyPrint and ignore unimportant symbols

--- RoslynPad/SymbolVisitorStuff/NamedTypeVisitor.cs
+++ RoslynPad/SymbolVisitorStuff/NamedTypeVisitor.cs
@@ -9,22 +9,47 @@ namespace RoslynPad.SymbolVisitorStuff
 {
     public class NamedTypeVisitor : SymbolVisitor
     {
+        int tabCount = 0;
         public override void VisitNamespace(INamespaceSymbol symbol)
         {
-            Console.WriteLine(symbol.ToString());
+            if (IsUnimportantSymbol(symbol))
+                return;
+
+            PrettyPrint(symbol);
+            tabCount++;
             foreach(var childSymbol in symbol.GetMembers())
             {
                 childSymbol.Accept(this);
             }
+            tabCount--;
         }
 
         public override void VisitNamedType(INamedTypeSymbol symbol)
         {
-            Console.WriteLine(symbol.ToString());
+            if (IsUnimportantSymbol(symbol))
+                return;
+
+            PrettyPrint(symbol);
+            tabCount++;
             foreach (var childSymbol in symbol.GetTypeMembers())
             {
                 childSymbol.Accept(this);
             }
+            tabCount--;
+        }
+
+        private void PrettyPrint(ISymbol symbol)
+        {
+            string tabs = new string('\t', tabCount);
+            Console.WriteLine(tabs + symbol.ToString());
+        }
+
+        private bool IsUnimportantSymbol(ISymbol symbol)
+        {
+            return symbol.Name.StartsWith("System")
+             || symbol.Name.StartsWith("Microsoft")
+             || symbol.Name.StartsWith("<")
+             ;
         }
     }
 }
