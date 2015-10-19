a1192652952780f791bea4750b13db9ed6370b6d
Josh Varty <joshvarty@gmail.com>
2015-10-19 13:57:50 -0400

    Added NamedTypeVisitor

--- RoslynPad/Program.cs
+++ RoslynPad/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using RoslynPad.SymbolVisitorStuff;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +13,28 @@ namespace RoslynPad
     {
         static void Main(string[] args)
         {
+            TestingSymbolVisitor();
+            Console.ReadKey();
+        }
+
+        public static void TestingSymbolVisitor()
+        {
+            var visitor = new NamedTypeVisitor();
+
+            var tree = CSharpSyntaxTree.ParseText(@"
+            class C
+            {
+                void M()
+                {
+                }
+            }");
+
+            var mscorlib = MetadataReference.CreateFromFile(typeof(object).Assembly.Location);
+            var compilation = CSharpCompilation.Create("MyCompilation",
+                syntaxTrees: new[] { tree }, references: new[] { mscorlib });
+
+
+            visitor.Visit(compilation.GlobalNamespace);
         }
     }
 }
---
+++ RoslynPad/SymbolVisitorStuff/NamedTypeVisitor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoslynPad.SymbolVisitorStuff
+{
+    public class NamedTypeVisitor : SymbolVisitor
+    {
+        public override void VisitNamespace(INamespaceSymbol symbol)
+        {
+            Console.WriteLine(symbol.ToString());
+            foreach(var childSymbol in symbol.GetMembers())
+            {
+                childSymbol.Accept(this);
+            }
+        }
+
+        public override void VisitNamedType(INamedTypeSymbol symbol)
+        {
+            Console.WriteLine(symbol.ToString());
+            foreach (var childSymbol in symbol.GetTypeMembers())
+            {
+                childSymbol.Accept(this);
+            }
+        }
+    }
+}
